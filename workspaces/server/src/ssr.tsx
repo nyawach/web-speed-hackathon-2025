37b509191951536ad166f144cc497e435aedd9a3
nyawach <yuunyawach@gmail.com>
2025-03-23 00:21:06 +0900

    chore: add memo

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -44,3 +44,12 @@ export function registerSsr(app: FastifyInstance): void {
     `);
   });
 }
+
+/** MEMO:
+      <script>
+        window.__staticRouterHydrationData = ${htmlescape({
+          actionData: context.actionData,
+          loaderData: context.loaderData,
+        })};
+      </script>
+ */

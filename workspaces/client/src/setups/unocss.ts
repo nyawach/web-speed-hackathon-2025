99bb868f1738c2b8c07bb77ec293aea66611fea6
nyawach <yuunyawach@gmail.com>
2025-03-22 16:05:36 +0900

    fix: use CDN

--- workspaces/client/src/setups/unocss.ts
+++ workspaces/client/src/setups/unocss.ts
@@ -48,19 +48,7 @@ async function init() {
       presets: [
         presetWind3(),
         presetIcons({
-          collections: {
-            bi: () => import('@iconify/json/json/bi.json').then((m): IconifyJSON => m.default as IconifyJSON),
-            bx: () => import('@iconify/json/json/bx.json').then((m): IconifyJSON => m.default as IconifyJSON),
-            'fa-regular': () =>
-              import('@iconify/json/json/fa-regular.json').then((m): IconifyJSON => m.default as IconifyJSON),
-            'fa-solid': () =>
-              import('@iconify/json/json/fa-solid.json').then((m): IconifyJSON => m.default as IconifyJSON),
-            fluent: () => import('@iconify/json/json/fluent.json').then((m): IconifyJSON => m.default as IconifyJSON),
-            'line-md': () =>
-              import('@iconify/json/json/line-md.json').then((m): IconifyJSON => m.default as IconifyJSON),
-            'material-symbols': () =>
-              import('@iconify/json/json/material-symbols.json').then((m): IconifyJSON => m.default as IconifyJSON),
-          },
+          cdn: 'https://esm.sh/'
         }),
       ],
     }),

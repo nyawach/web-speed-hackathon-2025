318c8ccf9507ae8c0ec95006b1f3603146328737
nyawach <yuunyawach@gmail.com>
2025-03-22 14:34:40 +0900

    Revert "feat!: remove hydration"
    This reverts commit a29fa2b26666182903aa31dd3b9899c107b1f70c.

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -64,6 +64,12 @@ export function registerSsr(app: FastifyInstance): void {
         </head>
         <body></body>
       </html>
+      <script>
+        window.__staticRouterHydrationData = ${htmlescape({
+          actionData: context.actionData,
+          loaderData: context.loaderData,
+        })};
+      </script>
     `);
   });
 }

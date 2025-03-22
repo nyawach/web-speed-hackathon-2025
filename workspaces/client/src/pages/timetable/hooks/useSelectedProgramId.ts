0bf3197187ef866c5377171666803b24f3a8e4fd
nyawach <yuunyawach@gmail.com>
2025-03-23 04:30:09 +0900

    chore: use selectProgram directly

--- workspaces/client/src/pages/timetable/hooks/useSelectedProgramId.ts
+++ workspaces/client/src/pages/timetable/hooks/useSelectedProgramId.ts
@@ -1,15 +1,6 @@
-import { StandardSchemaV1 } from '@standard-schema/spec';
-import * as schema from '@wsh-2025/schema/src/api/schema';
-import { ArrayValues } from 'type-fest';
-
 import { useStore } from '@wsh-2025/client/src/app/StoreContext';
-
-type Program = ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getTimetableResponse>>;
 
 export function useSelectedProgramId() {
   const timetablePage = useStore((s) => s.pages.timetable);
-  const setProgram = (program: Program | null) => {
-    timetablePage.selectProgram(program);
-  };
-  return [timetablePage.selectedProgramId, setProgram] as const;
+  return [timetablePage.selectedProgramId, timetablePage.selectProgram] as const;
 }

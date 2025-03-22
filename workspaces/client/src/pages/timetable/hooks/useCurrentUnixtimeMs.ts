ec1c04d964385b72da26ab1d51628bba9032b5b6
nyawach <yuunyawach@gmail.com>
2025-03-23 04:32:33 +0900

    chore: set current time at first

--- workspaces/client/src/pages/timetable/hooks/useCurrentUnixtimeMs.ts
+++ workspaces/client/src/pages/timetable/hooks/useCurrentUnixtimeMs.ts
@@ -5,6 +5,7 @@ import { useStore } from '@wsh-2025/client/src/app/StoreContext';
 export function useCurrentUnixtimeMs(): number {
   const timetablePage = useStore((s) => s.pages.timetable);
   useEffect(() => {
+    timetablePage.refreshCurrentUnixtimeMs();
     const interval = setInterval(() => {
       timetablePage.refreshCurrentUnixtimeMs();
     }, 30 * 1000);

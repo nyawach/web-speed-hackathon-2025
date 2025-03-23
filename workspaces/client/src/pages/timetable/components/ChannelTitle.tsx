e6452260c7cb49756913df9abba59389abc51a0b
nyawach <yuunyawach@gmail.com>
2025-03-23 18:20:23 +0900

    fix: use align

--- workspaces/client/src/pages/timetable/components/ChannelTitle.tsx
+++ workspaces/client/src/pages/timetable/components/ChannelTitle.tsx
@@ -16,8 +16,8 @@ export const ChannelTitle = ({ channelId }: Props) => {
 
   return (
     <div className="relative">
-      <div className={`border-x-solid h-[72px] w-auto border-x-[1px] border-x-[#212121] p-[14px]`} style={{ width }}>
-        <img alt={channel.name} className="object-contains size-full" draggable={false} src={channel.logoUrl} />
+      <div className={`border-x-solid h-[72px] w-auto border-x-[1px] border-x-[#212121] p-[14px] grid place-content-center`} style={{ width }}>
+        <img alt={channel.name} className="object-contains" draggable={false} src={channel.logoUrl} />
       </div>
 
       <div className="absolute inset-y-0 right-[-4px] z-10 w-[8px]">

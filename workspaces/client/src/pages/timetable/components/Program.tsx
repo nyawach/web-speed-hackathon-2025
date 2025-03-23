6f1a355464fa44267fb126704fb77cf9c1ed91c9
nyawach <yuunyawach@gmail.com>
2025-03-23 17:34:35 +0900

    feat: fetch when click button

--- workspaces/client/src/pages/timetable/components/Program.tsx
+++ workspaces/client/src/pages/timetable/components/Program.tsx
@@ -1,32 +1,22 @@
 import { StandardSchemaV1 } from '@standard-schema/spec';
 import * as schema from '@wsh-2025/schema/src/api/schema';
 import { DateTime } from 'luxon';
-import { ReactElement, useMemo, useRef, useState } from 'react';
+import { ReactElement, useMemo, useRef } from 'react';
 import { ArrayValues } from 'type-fest';
 
 import { Ellipsis } from '@wsh-2025/client/src/features/ellipsis/components/Ellipsis';
 import { Hoverable } from '@wsh-2025/client/src/features/layout/components/Hoverable';
-import { ProgramDetailDialog } from '@wsh-2025/client/src/pages/timetable/components/ProgramDetailDialog';
 import { useColumnWidth } from '@wsh-2025/client/src/pages/timetable/hooks/useColumnWidth';
 import { useCurrentUnixtimeMs } from '@wsh-2025/client/src/pages/timetable/hooks/useCurrentUnixtimeMs';
 
 interface Props {
   height: number;
+  onClick: (program: ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getTimetableResponse>>) => void;
   program: ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getTimetableResponse>>;
 }
 
-export const Program = ({ height, program }: Props): ReactElement => {
+export const Program = ({ height, onClick, program }: Props): ReactElement => {
   const width = useColumnWidth(program.channelId);
-
-  const [selectedProgramId, setProgramId] = useState<string | null>(null);
-  const shouldProgramDetailDialogOpen = program.id === selectedProgramId;
-  const onClick = () => {
-    setProgramId(program.id);
-  };
-  const onClose = () => {
-    setProgramId(null);
-  };
-
 
   const currentUnixtimeMs = useCurrentUnixtimeMs();
   const startAtMs = DateTime.fromISO(program.startAt).toMillis()
@@ -47,39 +37,36 @@ export const Program = ({ height, program }: Props): ReactElement => {
   }, [height, titleRef.current, imageRef.current, width]);
 
   return (
-    <>
-      <Hoverable classNames={{ hovered: isArchived ? 'hover:brightness-200' : 'hover:brightness-125' }}>
-        <button
-          className={`w-auto border-[1px] border-solid border-[#000000] ${isBroadcasting ? 'bg-[#FCF6E5]' : 'bg-[#212121]'} px-[12px] py-[8px] text-left ${isArchived ? 'opacity-50' : 'opacity-100'}`}
-          style={{ height, width }}
-          type="button"
-          onClick={onClick}
-        >
-          <div className="flex size-full flex-col overflow-hidden">
-            <div ref={titleRef} className="mb-[8px] flex flex-row items-start justify-start">
-              <span
-                className={`mr-[8px] shrink-0 grow-0 text-[14px] font-bold ${isBroadcasting ? 'text-[#767676]' : 'text-[#999999]'}`}
-              >
-                {DateTime.fromISO(program.startAt).toFormat('mm')}
-              </span>
-              <div
-                className={`grow-1 shrink-1 overflow-hidden text-[14px] font-bold ${isBroadcasting ? 'text-[#212121]' : 'text-[#ffffff]'}`}
-              >
-                <Ellipsis ellipsis reflowOnResize maxLine={3} text={program.title} visibleLine={3} />
-              </div>
-            </div>
-            <div className={`${shouldImageBeVisible ? 'visible' : 'hidden'} w-full`}>
-              <img
-                ref={imageRef}
-                alt=""
-                className="pointer-events-none w-full rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]"
-                src={program.thumbnailUrl}
-              />
+    <Hoverable classNames={{ hovered: isArchived ? 'hover:brightness-200' : 'hover:brightness-125' }}>
+      <button
+        className={`w-auto border-[1px] border-solid border-[#000000] ${isBroadcasting ? 'bg-[#FCF6E5]' : 'bg-[#212121]'} px-[12px] py-[8px] text-left ${isArchived ? 'opacity-50' : 'opacity-100'}`}
+        style={{ height, width }}
+        type="button"
+        onClick={() => { onClick(program); }}
+      >
+        <div className="flex size-full flex-col overflow-hidden">
+          <div ref={titleRef} className="mb-[8px] flex flex-row items-start justify-start">
+            <span
+              className={`mr-[8px] shrink-0 grow-0 text-[14px] font-bold ${isBroadcasting ? 'text-[#767676]' : 'text-[#999999]'}`}
+            >
+              {DateTime.fromISO(program.startAt).toFormat('mm')}
+            </span>
+            <div
+              className={`grow-1 shrink-1 overflow-hidden text-[14px] font-bold ${isBroadcasting ? 'text-[#212121]' : 'text-[#ffffff]'}`}
+            >
+              <Ellipsis ellipsis reflowOnResize maxLine={3} text={program.title} visibleLine={3} />
             </div>
           </div>
-        </button>
-      </Hoverable>
-      <ProgramDetailDialog isOpen={shouldProgramDetailDialogOpen} program={program} onClose={onClose} />
-    </>
+          <div className={`${shouldImageBeVisible ? 'visible' : 'hidden'} w-full`}>
+            <img
+              ref={imageRef}
+              alt=""
+              className="pointer-events-none w-full rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]"
+              src={program.thumbnailUrl}
+            />
+          </div>
+        </div>
+      </button>
+    </Hoverable>
   );
 };
--- workspaces/client/src/pages/timetable/components/ProgramList.tsx
+++ workspaces/client/src/pages/timetable/components/ProgramList.tsx
@@ -1,39 +1,57 @@
 import { StandardSchemaV1 } from '@standard-schema/spec';
 import * as schema from '@wsh-2025/schema/src/api/schema';
 import { DateTime } from 'luxon';
-import { ReactElement } from 'react';
+import { ReactElement, Suspense, useState } from 'react';
 import { ArrayValues } from 'type-fest';
 
 import { HEIGHT_ONE_HOUR } from '@wsh-2025/client/src/features/timetable/constants/grid_size';
 import { Gutter } from '@wsh-2025/client/src/pages/timetable/components/Gutter';
 import { Program } from '@wsh-2025/client/src/pages/timetable/components/Program';
+import { ProgramDetailDialog } from '@wsh-2025/client/src/pages/timetable/components/ProgramDetailDialog';
+
+type ProgramValue = ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getTimetableResponse>>
 
 interface Props {
   channelId: string;
-  programList: ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getTimetableResponse>>[];
+  programList: ProgramValue[];
 }
 
 export const ProgramList = ({ channelId, programList }: Props): ReactElement => {
+  const [selectedProgram, setSelectedProgram] = useState<ProgramValue | null>(null)
+
+  const onClick = (program: ProgramValue) => {
+    setSelectedProgram(program)
+  }
+
+  const onClose = () => {
+    setSelectedProgram(null)
+  }
+
   return (
-    <div className="relative">
-      <div className="flex flex-col">
-        {programList.map((program) => {
-          const startAt = DateTime.fromISO(program.startAt);
-          const endAt = DateTime.fromISO(program.endAt);
-          const duration = endAt.diff(startAt, 'minutes').minutes;
-          const height = HEIGHT_ONE_HOUR * (duration / 60);
+    <>
+      <div className="relative">
+        <div className="flex flex-col">
+          {programList.map((program) => {
+            const startAt = DateTime.fromISO(program.startAt);
+            const endAt = DateTime.fromISO(program.endAt);
+            const duration = endAt.diff(startAt, 'minutes').minutes;
+            const height = HEIGHT_ONE_HOUR * (duration / 60);
 
-          return (
-            <div key={program.id} className="shrink-0 grow-0">
-              <Program height={height} program={program} />
-            </div>
-          );
-        })}
-      </div>
+            return (
+              <div key={program.id} className="shrink-0 grow-0">
+                <Program height={height} program={program} onClick={onClick} />
+              </div>
+            );
+          })}
+        </div>
 
-      <div className="absolute inset-y-0 right-[-4px] z-10 w-[8px]">
-        <Gutter channelId={channelId} />
+        <div className="absolute inset-y-0 right-[-4px] z-10 w-[8px]">
+          <Gutter channelId={channelId} />
+        </div>
       </div>
-    </div>
+      {selectedProgram && (
+        <ProgramDetailDialog isOpen={!!selectedProgram} program={selectedProgram} onClose={onClose} />
+      )}
+    </>
   );
 };
